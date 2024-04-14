b4390feff6177aa41244ca0ba19313e96e113f1b
bgk <bogyeongkim614@gmail.com>
2024-04-14 18:22:45 +0900

    feat: 로그인 기능 추가

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axiox from 'axios';
 import '../../styles/LoginStyle/Login.css'; 
 // import { useDispatch } from 'react-redux';
 // import { loginUser } from '../../../_actions/user_action';
@@ -16,17 +17,34 @@ function Login() {
             setPassword(event.currentTarget.value);
         };
     
-        const onSubmitHandler = (event) => {
+        const onSubmitHandler = async (event) => {
             event.preventDefault();
-            console.log('Id:', Id);
-            console.log('Password:', Password);
+            try {
+              // 스프링 백엔드의 로그인 엔드포인트로 POST 요청 보내기
+              const response = await axiox.post('YOUR_SPRING_SERVER_ENDPOINT/login', {
+                username: Id, // 백엔드에서 사용하는 파라미터 이름에 맞추기
+                password: Password
+              });
+        
+              // 로그인 성공 처리
+              console.log(response.data);
+              // 예를 들어 로그인 후 토큰을 localStorage에 저장하거나 상태 관리 라이브러리에 저장
+              // localStorage.setItem('token', response.data.token);
+        
+              // 로그인 성공 후 페이지 리다이렉션
+              // history.push('/home'); // 'react-router-dom'의 useHistory 훅 사용시
+        
+            } catch (error) {
+              // 로그인 실패 처리
+              console.error('로그인 실패:', error);
+            }
         };
     
     return (
         <div className="form-container">
         <form className="login-form" onSubmit={onSubmitHandler}>
             <input 
-                type='id' 
+                type='text' 
                 className='form' 
                 value={Id} 
                 onChange={onIdHandler}
