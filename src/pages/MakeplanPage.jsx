f82b13de6415d887dec3b703e10ac4cbb85d9e18
bgk <bogyeongkim614@gmail.com>
2024-04-29 22:37:14 +0900

    feat: 일정만들기 페이지 구조 수정

--- src/pages/MakeplanPage.jsx
+++ src/pages/MakeplanPage.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "../styles/PageStyle/MakeplanPage.css"
 
 export default function MakeplanPage() {
-    return <h1> MakeplanPage </h1>;
+    return (
+        <div className="makeplanpage">
+            <h1> MakeplanPage </h1>
+            <div>질의응답 구현</div>
+            <div className="countbar">
+                질문 진행 정도
+                <div className="bar"></div>
+            </div>
+
+            <div className="qna">
+                <div className="q">질문</div>
+                <div className="a">답변</div>
+                <div className="ar">
+                    <div className="arrow">이전 질문</div>
+                    <div className="arrow">다음으로</div>
+                </div>
+            </div>
+            
+            <div className="reset_b">처음부터 다시하기</div>
+        </div>
+    )
 }
