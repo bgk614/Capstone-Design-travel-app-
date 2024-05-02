79421da5fdecf3f7a0ee6ce9f5dc1b682c5afa6b
Bogyeong Kim <bogyeongkim614@gmail.com>
2024-05-02 23:30:03 +0900

    feat: MypageNav 추가

---
+++ src/components/MypageNav.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import '../styles/MypageNavStyle/MypageNav.css';
+
+export default function MypageNav() {
+    return (
+        <div>
+            <div className="MypageNav">
+                    <Link className="mypage-nav-link" to='/best'>
+                        책갈피
+                    </Link>
+                    <Link className="mypage-nav-link" to='/destinations'>
+                        나의 여행 정보
+                    </Link>
+                    <Link className="mypage-nav-link" to='/plan'>
+                        일정 조회
+                    </Link>
+                    <Link className="mypage-nav-link" to='/board'>
+                        게시글 조회
+                    </Link>
+                    
+                    <Link className="mypage-nav-link" to='/faq'>
+                        나의 계정 설정
+                    </Link>
+            </div> 
+        </div>
+    );
+}
--- src/pages/MyPages/MyPage.jsx
+++ src/pages/MyPages/MyPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import MypageNav from "../../components/MypageNav";
 
 export default function MyPage() {
     return (
         <div>
             <div>
                 <h1> MyPage </h1>
-                <div>책갈피</div>
+                <MypageNav />
+                {/* <div>책갈피</div>
                 <div>나의 여행정보</div>
                 <div>일정 조회</div>
                 <div>게시글 조회</div>
                 <div>나의 계정 설정</div>
-                <div>회원정보수정</div>
+                <div>회원정보수정</div> */}
             </div>
             <div>
                 <div>최근여행</div>
